ced54f79e12479e4b2835e83f9b430e67d30c84f
linzhipeng <zhipeng.lin@ifchange.com>
2017-03-23 10:11:09 +0800

    取消上次修改

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,7 +2,7 @@ import {PureComponent} from 'react'
 import Page from '../Page'
 import Code from '../../components/Code'
 import { withRouter } from 'react-router'
-import {List,ListItem,Confirm,Button} from 'candy-mobile'
+import {List,ListItem} from 'candy-mobile'
 import load from '../../hoc/load'
 import menuListAction from '../../actions/menuListAction'
 
@@ -28,9 +28,6 @@ class MenuList extends PureComponent{
 export default ()=>{
     return (
         <Page name="home">
-            <Button onClick={()=>{
-                new Confirm(<a href="tel:18792677529">18792677529</a>);
-            }}>弹窗拨打电话</Button>
             <div className="ct-page__title">开始使用：</div>
             <Code lang="bash">{'npm install candy-mobile'}</Code>
             <div className="ct-page__title">组件列表：</div>
